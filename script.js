2c73c49c5e1430a0a3a90e168d67ee25640fca0b
Intelligence247 <115150772+Intelligence247@users.noreply.github.com>
2023-01-09 22:18:11 +0100

    fixes

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ let quotes = {
         'Life is too short and sweet to be spent by cribbing and complaining about things. Here are some random quotes about the most wonderful gift that we\'ve got',
         'Humor is richly rewarding to the person who employs it. It has some value in gaining and holding attention. But it has no persuasive value at all',
         'The price of anything is the amount of life you exchange for it.',
-        'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tenetur sint iste optio placeat dolor labore, reprehenderit ipsum aspernatur blanditiis maiores.',
         'A critic is someone who never actually goes to the battle, yet who afterwards comes out shooting the wounded.'
     ],
     writers: [
@@ -14,11 +13,10 @@ let quotes = {
         'Life',
         'John Kenneth Galbraith',
         'David Thoreau',
-        '- Author',
         'Tyne Daly'
     ]
 }
-
+console.log(quotes.writers.length, quotes.quote.length)
 let btn = document.querySelector('button')
 const qq = document.querySelector('#qq')
 let author = document.querySelector('#author')
